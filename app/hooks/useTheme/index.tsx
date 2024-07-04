29bcae6e793e59d88ff7a762cec626ee54029834
tkx.cool <tkx.cool@gmail.com>
2024-07-05 03:43:38 +0800

    feat: add useTheme hook logic

--- app/hooks/useTheme/index.tsx
+++ app/hooks/useTheme/index.tsx
@@ -1,5 +1,11 @@
+import { useNavigation, useRouteLoaderData } from '@remix-run/react'
+import { ColorScheme } from '~/lib/color-scheme.server'
+
 export const useTheme = () => {
-	// const { setting, setSetting } = useSettingStore()
-	// const initial = useRef(true)
-	return <div>useTheme</div>
+	const rootLoaderData = useRouteLoaderData<any>('root')
+	const rootColorScheme = rootLoaderData?.colorScheme ?? 'system'
+
+	const { formData } = useNavigation()
+	const optimisticColorScheme = formData?.has('colorScheme') ? (formData.get('colorScheme') as ColorScheme) : null
+	return optimisticColorScheme || rootColorScheme
 }
